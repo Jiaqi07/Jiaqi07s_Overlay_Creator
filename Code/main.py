73bc3c8d6c0accf0c297b16334a11248496080ae
陈家齐 <alanjiach@gmail.com>
2022-06-06 13:59:36 -0500

    Update main.py
    Improved Update with bug fixes

--- Code/main.py
+++ Code/main.py
@@ -1,4 +1,6 @@
+import numpy as np
 from PIL import Image, ImageOps, ImageFilter
+from colorthief import ColorThief
 
 DEFAULT_IMG = Image.open('images/Default.jpg')  # Any Size just plug into Cube The Square (Future replicate it)
 # Default Images - 2048x2048 each image
@@ -10,8 +12,9 @@ DEFAULT_RIGHT = Image.open('images/Default_Right.bmp')  # Directly
 DEFAULT_TOP = Image.open('images/Default_Top.bmp')
 
 STARFIELD031 = Image.open('images/starfield031.png')
-TRANSITIONOVERLAY = Image.open('images/TRANSITIONOVERLAY.png')
-
+TRANSITIONOVERLAY = Image.open('images/TransitionOverlay.png')
+color_thief = ColorThief('images/Default.jpg')
+dominant_color = color_thief.get_color(quality=1)
 
 def show(file_image):
     file_image.show()
@@ -21,12 +24,16 @@ def smooth(file_image):
     return file_image.filter(ImageFilter.SMOOTH)
 
 
+def moving_average(x, w):
+    return np.convolve(x, np.ones(w), 'valid') / w
+
+
 def get_file():
     # Read image file path from user, or use the default file
     file_image = input('Enter image file (or press enter for default): ')
     if file_image == '':
         file_image = DEFAULT_IMG
-    else:  # figure out how to set the image to the user's one!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    else: 
         file_image = DEFAULT_IMG
 
     print("")
@@ -45,12 +52,24 @@ def start_overlay():
     Transition.paste(DEFAULT_TOP, (0, 0))
     Transition.paste(DEFAULT_FRONT, (0, 2048))
     Transition.paste(DEFAULT_BOTTOM, (0, 4096))
-    Transition.show()
-    # Part 2
 
-    Sky = Transition.copy().filter(ImageFilter.GaussianBlur())
+    print(Transition.size)
 
-    # TransCrop = Transition.copy().crop((0, 0, 1024, 6144))
+    npPic = np.array(Transition)
+
+    for i in range(0, 6144):
+        for j in range(1020, 1028):
+            if npPic[i][j][0] > 250 and npPic[i][j][1] > 250 and npPic[i][j][2] > 250:
+                npPic[i][j][0] = dominant_color[0]
+                npPic[i][j][1] = dominant_color[1]
+                npPic[i][j][2] = dominant_color[2]
+
+    pil_image = Image.fromarray(npPic)
+    # pil_image.show()
+
+    Transition = pil_image.filter(ImageFilter.GaussianBlur())
+
+    # TransCrop = Transition.copy().crop((512, 512, 1024, 6144))
     # TransInvert = ImageOps.mirror(TransCrop)
     # Transition.paste(TransInvert, (1024, 0))
 
@@ -63,9 +82,7 @@ def start_overlay():
     Sky.paste(Transition.copy().crop((0, 2048, 2048, 4096)), (2048, 2048))
     Sky.paste(Transition.copy().crop((0, 4096, 2048, 6144)), (0, 0))
 
-    Sky.show()
-
-    # Sky.save('D:\starfield031.png')
+    Sky.save('out\starfield031.png')
 
 
 if __name__ == '__main__':
